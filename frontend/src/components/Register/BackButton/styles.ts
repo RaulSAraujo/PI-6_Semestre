6fa4f558d910c061a9004cb569e7da053addd4d9
RaulSAraujo <raul.araujo.2000@hotmail.com>
2025-06-06 22:32:09 -0300

    💄 styled: size button

--- frontend/src/components/Register/BackButton/styles.ts
+++ frontend/src/components/Register/BackButton/styles.ts
@@ -1,17 +1,17 @@
 import { Button, styled, alpha } from "@mui/material";
 
 export const BackButtonStyled = styled(Button)(({ theme }) => ({
+  zIndex: 10,
+  minWidth: "auto",
+  borderRadius: 12,
   position: "absolute",
   top: theme.spacing(2),
   left: theme.spacing(2),
-  borderRadius: 12,
-  padding: theme.spacing(1),
-  minWidth: "auto",
-  backgroundColor: alpha(theme.palette.background.paper, 0.7),
-  color: theme.palette.text.primary,
+  padding: theme.spacing(2),
   backdropFilter: "blur(5px)",
+  color: theme.palette.text.primary,
   boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-  zIndex: 10,
+  backgroundColor: alpha(theme.palette.background.paper, 0.7),
   "&:hover": {
     backgroundColor: alpha(theme.palette.background.paper, 0.9),
   },
